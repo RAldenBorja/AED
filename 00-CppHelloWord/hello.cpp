35e01a40eadf2a51b7a9563de17193239e83bd7f
RAldenBorja <rborjaagreda@frba.utn.edu.ar>
2026-04-29 16:57:21 -0300

    Update hello.cpp

--- 00-CppHelloWord/hello.cpp
+++ 00-CppHelloWord/hello.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-//g++ -std=c++23 hello.cpp -o main
-//.\main
+
 int main() {
-    std::cout << "Hola mundo";
+    std::cout << "Hola mundo \n";
+    std::cout << "- Alden Borja \n- K1053 \n- Grupo 6";
     return 0;
 }
